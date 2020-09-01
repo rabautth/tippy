d412456623906df98d49a57554a302cb7ecb3fb3
Tommy Rabaut <rabautth@msu.edu>
2020-09-01 14:50:23 -0400

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by tommy rabaut on 9/1/20.
+//  Copyright © 2020 tommy rabaut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
